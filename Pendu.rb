f8d65a77202c5d1e717fb632c8e8fcf043d0965f
Adam Castro Guzman <castroguzman033@gmail.com>
2019-05-31 12:21:35 +0200

    Translation into english

--- Pendu.rb
+++ Pendu.rb
@@ -49,19 +49,19 @@ def getting_a_letter(letters_list, lifes, hidden_word, word_to_find)
   guess = ""
 
   while guess == "" || guess == "\n" || (guess.length != word_to_find.length && guess.length != 1)
-    ask_user_to_give_letter(lifes, hidden_word, letters_list, "Entrez une lettre ou un mot entier : ", word_to_find)
+    ask_user_to_give_letter(lifes, hidden_word, letters_list, "Suggest a letter or a full word: ", word_to_find)
     guess = gets.downcase.chomp.strip
   end
 
   while letters_list.include?(guess)
-    ask_user_to_give_letter(lifes, hidden_word, letters_list, "Vous avez déjà joué cette lettre, essayez à nouveau : ", word_to_find)
+    ask_user_to_give_letter(lifes, hidden_word, letters_list, "You already suggested this letter, try again: ", word_to_find)
     guess = gets.downcase.chomp.strip
   end
 
   while guess.length > 1 && guess != word_to_find
     lifes = lifes - 1 
     letters_list.push(guess)
-    ask_user_to_give_letter(lifes, hidden_word, letters_list, "'#{guess}' n'est pas le mot recherché, essayez à nouveau : ", word_to_find)
+    ask_user_to_give_letter(lifes, hidden_word, letters_list, "'#{guess}' is not the good word, try again: ", word_to_find)
     guess = gets.downcase.chomp.strip
   end
 
@@ -74,8 +74,8 @@ end
 
 def ask_user_to_give_letter(lifes, hidden_word, letters_list, prompt, word_to_find)
   clear()
-  puts "Vous avez #{lifes} vies : #{hidden_word}"
-  print "Les lettres que vous avez tentés : "
+  puts "You have #{lifes} lives: #{hidden_word}"
+  print "Suggested: "
   letters_list.each do |letter|
     if letter_in_word_to_find(word_to_find, letter)
       print letter.green + ", "
@@ -100,11 +100,13 @@ end
 while true
   round_running = true
   word_to_find = ""
+
   while word_to_find == "" || word_to_find == "\n" || word_to_find.split().length == 0
     clear()
-    print "Indiquez un mot que le joueur devra trouver : "
+    print "Set the word to guess: "
     word_to_find = gets.chomp.downcase
   end
+
   word_to_find = word_to_find.split()
   word_to_find = word_to_find.join("")
   hidden_word = create_hidden_word(word_to_find)
@@ -125,14 +127,14 @@ while true
 
     if guess == word_to_find || hidden_word.count("_") == 0
       clear()
-      puts "Féliciation, vous avez gagné ! Le mot était #{word_to_find.upcase.green} !"
+      puts "Congratulation, you won! The word was: #{word_to_find.upcase.green} !"
       continue?()
       round_running = false
     end
 
     if lifes == 0
       clear()
-      puts "Vous avez lamentablement échoué ! Le mot était #{word_to_find.upcase.green} !"
+      puts "You failed! The word was: #{word_to_find.upcase.green} !"
       continue?()
       round_running = false
     end
